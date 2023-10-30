c34b37b31e7ac6b16e615bc00845f68eccb3c52a
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2023-10-30 23:39:40 +0000

    more nvim plugins

--- .config/nvim/lua/vincent/plugins/lualine.lua
+++ .config/nvim/lua/vincent/plugins/lualine.lua
@@ -61,6 +61,24 @@ return {
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
           },
+          {
+            function()
+              local icon = require("lazyvim.config").icons.kinds.Copilot
+              local status = require("copilot.api").status.data
+              return icon .. (status.message or "")
+            end,
+            cond = function()
+              local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+              return ok and #clients > 0
+            end,
+            color = function()
+              if not package.loaded["copilot"] then
+                return
+              end
+              local status = require("copilot.api").status.data
+              return colors[status.status] or colors[""]
+            end,
+          },
           { "encoding" },
           { "fileformat" },
           { "filetype" },
@@ -69,4 +87,3 @@ return {
     })
   end,
 }
-
