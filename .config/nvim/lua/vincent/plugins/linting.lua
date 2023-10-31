d0b6e9e5610566b987d42866caee159fc1191419
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2023-10-31 23:43:54 +0000

    comments, no autolint

--- .config/nvim/lua/vincent/plugins/linting.lua
+++ .config/nvim/lua/vincent/plugins/linting.lua
@@ -16,16 +16,17 @@ return {
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+    --[[ Comment out the autocmd section to disable automatic linting
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
     })
+    --]]
 
     vim.keymap.set("n", "<leader>l", function()
       lint.try_lint()
     end, { desc = "Trigger linting for current file" })
   end,
 }
-
