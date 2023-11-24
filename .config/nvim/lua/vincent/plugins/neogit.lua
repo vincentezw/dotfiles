5c26c8b446a6be7e90af3438c9b17a1fc7cadcb4
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2023-11-24 00:13:08 +0000

    Add Neogit, gitsigns

---
+++ .config/nvim/lua/vincent/plugins/neogit.lua
@@ -0,0 +1,21 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "nvim-telescope/telescope.nvim", -- optional
+    "sindrets/diffview.nvim", -- optional
+    "ibhagwan/fzf-lua", -- optional
+  },
+  config = function()
+    require("neogit").setup({
+      integrations = {
+        diffview = true,
+        telescope = true,
+      },
+    })
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>ng", "<cmd>Neogit<cr>", { desc = "Neogit" })
+    keymap.set("n", "<leader>nc", "<cmd>Neogit commit<cr>", { desc = "Neogit commit" })
+  end,
+}
