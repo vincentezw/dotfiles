72b223adc9859642e333cf5a651b83da9237174b
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2024-03-06 00:18:49 +0000

    smartyank

--- .config/nvim/lua/vincent/plugins/osc.lua
+++ .config/nvim/lua/vincent/plugins/osc.lua
@@ -1,12 +1,13 @@
 return {
-  "ojroques/nvim-osc52",
-  config = function()
-    local copy = function()
-      if vim.v.event.operator == "y" and vim.fn.has("clipboard") == 0 then
-        require("osc52").copy_register("+")
-      end
-    end
-
-    vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
-  end,
+  -- "ojroques/nvim-osc52",
+  -- config = function()
+  --   local copy = function()
+  --     if vim.v.event.operator == "y" and vim.fn.has("clipboard") == 0 then
+  --       require("osc52").copy_register("+")
+  --     end
+  --   end
+  --
+  --   vim.api.nvim_create_autocmd("TextYankPost", { callback = copy })
+  -- end,
+  'ibhagwan/smartyank.nvim',
 }
