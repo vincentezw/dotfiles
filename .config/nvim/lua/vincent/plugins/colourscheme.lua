bd9490801a46c21eae51cd82bf17dc5d03df8a31
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2024-04-29 23:41:47 +0100

    wofi, hypr and wlogout

--- .config/nvim/lua/vincent/plugins/colourscheme.lua
+++ .config/nvim/lua/vincent/plugins/colourscheme.lua
@@ -15,6 +15,7 @@ return {
           hi BufferCurrentSign guifg=#161616 guibg=#2E2E2E
           hi BufferTabpageFill guibg=#161616
           hi BufferInactive guibg=#161616
+          hi BufferInactiveMod guibg=#161616 guifg=#ff9e64
           hi BufferInactiveSign guifg=#161616 guibg=#161616
 
           hi NeoTreeTabActive gui=italic
