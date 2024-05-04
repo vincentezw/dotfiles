21603e53727b54557fdf7dbdb050b54aac07229a
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2024-05-04 23:26:40 +0100

    add kitty bg

--- .config/nvim/lua/vincent/plugins/colourscheme.lua
+++ .config/nvim/lua/vincent/plugins/colourscheme.lua
@@ -13,10 +13,11 @@ return {
           hi BufferCurrent gui=italic guibg=#2E2E2E
           hi BufferCurrentMod gui=italic guifg=#ff9e64 guibg=#2E2E2E
           hi BufferCurrentSign guifg=#161616 guibg=#2E2E2E
-          hi BufferTabpageFill guibg=#161616
           hi BufferInactive guibg=#161616
           hi BufferInactiveMod guibg=#161616 guifg=#ff9e64
           hi BufferInactiveSign guifg=#161616 guibg=#161616
+          hi BufferTabpageFill guibg=#161616
+
 
           hi NeoTreeTabActive gui=italic
           hi NeoTreeTabInactive guibg=#161616
