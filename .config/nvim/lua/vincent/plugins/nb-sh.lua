67fe1f901cbb21064a394b535d37b02d6af3f97f
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2024-05-13 01:14:03 +0100

    nb plugin

--- .config/nvim/lua/vincent/plugins/nb-sh.lua
+++ .config/nvim/lua/vincent/plugins/nb-sh.lua
@@ -1,8 +1,15 @@
 return {
-  -- "vincentezw/nb-sh.nvim",
-  dir = "/home/vincent/src/nb.nvim/",
+  "vincentezw/nb-sh.nvim",
+  -- dir = "/home/vincent/src/nb.nvim/",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    -- vim.notify("yeah", vim.log.levels.INFO)
+    local nb = require("nb")
+    nb.setup({
+      auto_sync = true,
+    })
+
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>nn", "<cmd>NbNew<CR>", { desc = "New note" })
+    keymap.set("n", "<leader>nb", "<cmd>Nbbrowse<CR>", { desc = "Browse notes" })
   end,
 }
--- .config/nvim/lua/vincent/plugins/tmux-navigator.lua
+++ .config/nvim/lua/vincent/plugins/tmux-navigator.lua
@@ -4,15 +4,16 @@ return {
     local nvim_tmux_nav = require('nvim-tmux-navigation')
 
     nvim_tmux_nav.setup {
-        disable_when_zoomed = false
+      disable_when_zoomed = false,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      }
     }
-
-    vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-    vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-    vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-    vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-    vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-    vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
   end
 }
 
