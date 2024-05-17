417140f631746fc68dd30951b24974d87d605cb6
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2024-05-17 12:05:32 +0100

    install neovim, path preference

--- install.sh
+++ install.sh
@@ -17,8 +17,7 @@ ln -sf ${HOME}/dotfiles/.npmrc ${HOME}/.npmrc
 
 if [ -n "$SPIN" ]; then
   sudo add-apt-repository -y ppa:neovim-ppa/unstable
-  sudo apt-get update
-  apt_packages=("exa" "tmux" "fzf")
+  apt_packages=("exa" "tmux" "fzf" "neovim")
   for package in "${apt_packages[@]}"; do
     if ! dpkg -l | grep -q "$package"; then
       sudo apt-get install -y "$package"
@@ -26,20 +25,20 @@ if [ -n "$SPIN" ]; then
       echo "$package is already installed."
     fi
   done
-fi
 
-
-if [ -n "$SPIN" ]; then
   sh ${HOME}/dotfiles/starship.sh -f
   sh ${HOME}/dotfiles/oh-my-zsh.sh --unattended --keep-zshrc
 
-  # gem_user_install_dir=$(gem environment | grep -oP 'USER INSTALLATION DIRECTORY: \K.*')
-  # gem_bin_path="$gem_user_install_dir/bin"
+  gem_user_install_dir=$(gem environment | grep -oP 'USER INSTALLATION DIRECTORY: \K.*')
+  gem_bin_path="$gem_user_install_dir/bin"
   # gem install --user neovim ruby-lsp
+  gem install --user neovim
 
   if [ -d "$gem_bin_path" ]; then
-    echo "export PATH=\"$gem_bin_path:\$PATH\"" >> ~/.zshrc
+    echo "export PATH=\"$gem_bin_path:/usr/bin:\$PATH\"" >> ~/.zshrc
     echo "Gem bin path added to PATH."
+  else
+    echo "export PATH=\"/usr/bin:\$PATH\"" >> ~/.zshrc
   fi
 else
   sudo gem install neovim
@@ -48,6 +47,5 @@ npm install -g neovim nb.sh
 sudo "$(which nb)" completions install
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/vincentezw/notes.git ~/.nb  
 
-${HOME}/dotfiles/nb-setup.hs
+${HOME}/dotfiles/nb-setup.sh
