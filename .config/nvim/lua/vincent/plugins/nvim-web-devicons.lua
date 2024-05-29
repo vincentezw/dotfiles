5d4bf430f417f136aada41552c62ac6831bf45fe
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2024-05-29 10:22:27 +0100

    gitsigns keymaps, fix icons config

--- .config/nvim/lua/vincent/plugins/nvim-web-devicons.lua
+++ .config/nvim/lua/vincent/plugins/nvim-web-devicons.lua
@@ -1,17 +1,16 @@
 return {
   "DaikyXendo/nvim-material-icon",
-  -- "nvim-tree/nvim-web-devicons",
   config = function()
-    require("nvim-material-icon").setup({
+    require("nvim-web-devicons").setup({
       default = true,
+      override = {
+        gql = {
+          icon = "",
+          color = "#e535ab",
+          cterm_color = "199",
+          name = "GraphQL",
+        },
+      },
     })
-    -- require("nvim-web-devicons").set_icon({
-    --   gql = {
-    --     icon = "",
-    --     color = "#e535ab",
-    --     cterm_color = "199",
-    --     name = "GraphQL",
-    --   },
-    -- })
   end,
 }
