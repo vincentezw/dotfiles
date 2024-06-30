b796a1d5b9515107c85f98b37e5c428363160c0a
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2024-07-01 00:20:47 +0100

    markdown

--- .config/nvim/lua/vincent/plugins/goto-preview.lua
+++ .config/nvim/lua/vincent/plugins/goto-preview.lua
@@ -9,7 +9,11 @@ return {
       preview_window_title = { enable = true, position = "center" },
       post_open_hook = function(buffer, window)
         -- Override the highlight group for the title text
-        vim.api.nvim_win_set_option(window, "winhighlight", "Title:GotoPreviewTitle")
+        -- this used to target 'window' but I think it's fine
+        -- vim.api.nvim_win_set_option(window, "winhighlight", "Title:GotoPreviewTitle")
+        vim.api.nvim_set_option_value("winhighlight", "Title:GotoPreviewTitle", {})
+        -- vim.api.nvim_buf_set_keymap(buffer, 'n', '1', 'lua require("goto-preview").close_all_win()', {noremap = true, silent = true})
+        vim.api.nvim_buf_set_keymap(buffer, 'n', 'q', ':bd<CR>', {noremap = true, silent = true})
       end,
     })
   end,
