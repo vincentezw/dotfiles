d7bd682ca3e53debe8a66def5c3815bde84188df
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2024-08-04 01:16:36 +0100

    kulala

---
+++ .config/nvim/lua/vincent/plugins/kulala.lua
@@ -0,0 +1,17 @@
+return {
+  'mistweaverco/kulala.nvim',
+  config = function()
+    require('kulala').setup()
+
+    vim.filetype.add({
+      extension = {
+        ['http'] = 'http',
+      },
+    })
+
+    local opts = { noremap = true, silent = true }
+    opts.desc = 'Fire request with kulala'
+    -- vim.api.nvim_set_keymap("n", "<C-j>", ":lua require('kulala').jump_next()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("n", "<C-l>", ":lua require('kulala').run()<CR>", opts)
+  end
+}
