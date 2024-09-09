1ecc29a60383d4b280e769cefda9a0cf47ae475e
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2024-09-09 11:48:06 +0100

    change kulala binding

--- .config/nvim/lua/vincent/plugins/kulala.lua
+++ .config/nvim/lua/vincent/plugins/kulala.lua
@@ -12,6 +12,6 @@ return {
     local opts = { noremap = true, silent = true }
     opts.desc = 'Fire request with kulala'
     -- vim.api.nvim_set_keymap("n", "<C-j>", ":lua require('kulala').jump_next()<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap("n", "<C-l>", ":lua require('kulala').run()<CR>", opts)
+    vim.api.nvim_set_keymap("n", "<leader>k", ":lua require('kulala').run()<CR>", opts)
   end
 }
