8bc6ee0037f57dcc9b497c33172122feb4181736
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2025-01-07 12:11:59 +0000

    fix fonts, lsp to fzf-lua

--- .config/nvim/lua/vincent/plugins/codetools/lspconfig.lua
+++ .config/nvim/lua/vincent/plugins/codetools/lspconfig.lua
@@ -32,15 +32,19 @@ return {
       opts.buffer = bufnr
       -- set keybinds
       opts.desc = "Show LSP references"
-      keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      -- keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      keymap.set("n", "gR", "<cmd>lua require('fzf-lua').lsp_references()<CR>", opts) -- show definition, references
       opts.desc = "Go to declaration"
       keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
       opts.desc = "Show LSP definitions"
-      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      -- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      keymap.set("n", "gd", "<cmd>lua require('fzf-lua').lsp_definitions()<CR>", opts) -- show lsp definitions
       opts.desc = "Show LSP implementations"
-      keymap.set("n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+      -- keymap.set("n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+      keymap.set("n", "gI", "<cmd>lua require('fzf-lua').lsp_implementations()<CR>", opts) -- show lsp implementations
       opts.desc = "Show LSP type definitions"
-      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+      -- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+      keymap.set("n", "gt", "<cmd>lua require('fzf-lua').lsp_type_definitions()<CR>", opts) -- show lsp type definitions
       opts.desc = "See available code actions"
       keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
       opts.desc = "Smart rename"
