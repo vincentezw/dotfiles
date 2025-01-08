934e0f3fd53ce90c04ba23f685cb38e14a213d1a
Vincent Zwanenburg <vincent.zwanenburg@shopify.com>
2025-01-08 10:24:50 +0000

    fix tmux, lsp diagnostics

--- .config/nvim/lua/vincent/plugins/codetools/lspconfig.lua
+++ .config/nvim/lua/vincent/plugins/codetools/lspconfig.lua
@@ -32,25 +32,22 @@ return {
       opts.buffer = bufnr
       -- set keybinds
       opts.desc = "Show LSP references"
-      -- keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
       keymap.set("n", "gR", "<cmd>lua require('fzf-lua').lsp_references()<CR>", opts) -- show definition, references
       opts.desc = "Go to declaration"
       keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
       opts.desc = "Show LSP definitions"
-      -- keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
       keymap.set("n", "gd", "<cmd>lua require('fzf-lua').lsp_definitions()<CR>", opts) -- show lsp definitions
       opts.desc = "Show LSP implementations"
-      -- keymap.set("n", "gI", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
       keymap.set("n", "gI", "<cmd>lua require('fzf-lua').lsp_implementations()<CR>", opts) -- show lsp implementations
       opts.desc = "Show LSP type definitions"
-      -- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
       keymap.set("n", "gt", "<cmd>lua require('fzf-lua').lsp_type_definitions()<CR>", opts) -- show lsp type definitions
       opts.desc = "See available code actions"
       keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
       opts.desc = "Smart rename"
       keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
       opts.desc = "Show buffer diagnostics"
-      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+      -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+      keymap.set("n", "<leader>D", "<cmd>lua require('fzf-lua').lsp_diagnostics({ bufnr = 0 })<CR>", opts) -- show diagnostics for file
       opts.desc = "Show line diagnostics"
       keymap.set("n", "<leader>d", function() vim.diagnostic.open_float({border = 'rounded'}) end, opts)
       opts.desc = "󰒮 diagnostic"
