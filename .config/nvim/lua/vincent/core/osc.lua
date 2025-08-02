d246fc4029ab777d45b007d70255519f393c291a
Vincent Elger Zwanenburg <vincent@zwanenburg.ie>
2025-08-02 09:31:55 +0100

    increate timeout for yank hilight

--- .config/nvim/lua/vincent/core/osc.lua
+++ .config/nvim/lua/vincent/core/osc.lua
@@ -51,7 +51,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank {
       higroup = "IncSearch",
-      timeout = 400,
+      timeout = 800,
     }
   end,
 })
